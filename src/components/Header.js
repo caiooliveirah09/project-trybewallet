9ac9f352df7a6949ce14e74d903a24509865e7c0
caiooliveirah09 <leelros@hotmail.com>
2022-08-02 14:42:02 -0300

    i solved requirement 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Header extends Component {
+class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <>
+        <h2 data-testid="email-field">{ email }</h2>
+        <h2 data-testid="total-field">0</h2>
+        <h2 data-testid="header-currency-field">BRL</h2>
+        <span>Header</span>
+      </>
     );
   }
 }
+
+const mapStateToProps = (storage) => ({
+  email: storage.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Header);
