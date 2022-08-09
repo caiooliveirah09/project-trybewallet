87ae384c32f08aab573059a26a4dc46c3f8c6b66
caiooliveirah09 <leelros@hotmail.com>
2022-08-09 19:01:39 -0300

    i solved requirement 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Table extends Component {
+class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table>
-        <tr>
-          <th>Descrição </th>
-          <th>Tag </th>
-          <th>Método de pagamento </th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Descrição </th>
+            <th>Tag </th>
+            <th>Método de pagamento </th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map(
+            ({
+              id, description, tag, method, value, exchangeRates, currency,
+            }) => (
+              <tr key={ id }>
+                <td>{ description }</td>
+                <td>{ tag }</td>
+                <td>{ method }</td>
+                <td>{ Number(value).toFixed(2) }</td>
+                <td>{ exchangeRates[currency].name }</td>
+                <td>{ Number(exchangeRates[currency].ask).toFixed(2) }</td>
+                <td>{ (value * exchangeRates[currency].ask).toFixed(2) }</td>
+                <td>Real</td>
+              </tr>
+            ),
+          )}
+        </tbody>
       </table>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+};
+
+export default connect(mapStateToProps, null)(Table);
